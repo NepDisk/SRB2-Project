2e296f6ee48688de2b3e862fac4aafaf7b13fbea
ashifolfi <anticream39@yahoo.com>
2022-11-06 13:50:26 -0500

    rename skynum to changesky

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -909,7 +909,7 @@ void D_RegisterClientCommands(void)
 	COM_AddCommand("charspeed", Command_Charspeed_f);
 	COM_AddCommand("teleport", Command_Teleport_f);
 	COM_AddCommand("rteleport", Command_RTeleport_f);
-	COM_AddCommand("skynum", Command_Skynum_f);
+	COM_AddCommand("changesky", Command_Skynum_f);
 	COM_AddCommand("weather", Command_Weather_f);
 	COM_AddCommand("toggletwod", Command_Toggletwod_f);
 #ifdef _DEBUG
