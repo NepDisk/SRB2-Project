175e080cc65607f237e8de699c50c06783d10a0e
SMS Alfredo <65426124+SMS-Alfredo@users.noreply.github.com>
2024-05-21 15:29:54 -0500

    Invert and Fix RF_FLIPOFFSETS Behavior

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4483,6 +4483,8 @@ static void HWR_ProjectSprite(mobj_t *thing)
 	}
 #endif
 
+	SINT8 hflipoffset = 1;
+	SINT8 vflipoffset = 1;
 	if (thing->renderflags & RF_ABSOLUTEOFFSETS)
 	{
 		spr_offset = interp.spritexoffset;
@@ -4490,19 +4492,19 @@ static void HWR_ProjectSprite(mobj_t *thing)
 	}
 	else
 	{
-		SINT8 flipoffset = 1;
+		if (!(thing->renderflags & RF_FLIPOFFSETS) && flip)
+			hflipoffset = -1;
+		if (!(thing->renderflags & RF_FLIPOFFSETS) && R_ThingVerticallyFlipped(thing))
+			vflipoffset = -1;
 
-		if ((thing->renderflags & RF_FLIPOFFSETS) && flip)
-			flipoffset = -1;
-
-		spr_offset += interp.spritexoffset * flipoffset;
-		spr_topoffset += interp.spriteyoffset * flipoffset;
+		spr_offset += interp.spritexoffset * hflipoffset;
+		spr_topoffset += interp.spriteyoffset * vflipoffset;
 	}
 	
 #ifdef ROTSPRITE
 	// let spriteinfo offset by units less than a pixel
-	spr_offset += (fixed_t)sprinfo->offset[thing->frame&FF_FRAMEMASK].x;
-	spr_topoffset += (fixed_t)sprinfo->offset[thing->frame&FF_FRAMEMASK].y;
+	spr_offset += (fixed_t)sprinfo->offset[thing->frame&FF_FRAMEMASK].x * hflipoffset;
+	spr_topoffset += (fixed_t)sprinfo->offset[thing->frame&FF_FRAMEMASK].y * vflipoffset;
 #endif
 
 	if (papersprite)
