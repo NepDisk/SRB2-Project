0e63c97959f8414b4c40d2700b27a782976072e1
NepDisk <16447892+NepDisk@users.noreply.github.com>
2024-06-01 16:17:59 -0400

    Up freeslot limit

--- src/info.h
+++ src/info.h
@@ -572,7 +572,7 @@ void A_ChangeHeight();
 extern int actionsoverridden[NUMACTIONS][MAX_ACTION_RECURSION];
 
 // ratio of states to sprites to mobj types is roughly 6 : 1 : 1
-#define NUMMOBJFREESLOTS 1024
+#define NUMMOBJFREESLOTS 4096
 #define NUMSPRITEFREESLOTS NUMMOBJFREESLOTS
 #define NUMSTATEFREESLOTS (NUMMOBJFREESLOTS*8)
 #define MAXSPRITENAME 64
